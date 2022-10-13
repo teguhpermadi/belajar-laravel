b3a16aca0c7d09f3706bf5d6cd675c14ad7a3164
Teguh Permadi <tguh.permadi@gmail.com>
2022-10-14 05:36:40 +0700

    mapel

---
+++ database/seeders/MapelSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Mapel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MapelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Mapel::factory(5)->create();
+    }
+}
